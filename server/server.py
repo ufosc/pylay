22022f44ea2b6d490dfc9a1891687f6ae71c75ac
Wyatt Lindquist <git.wquist@gmail.com>
2016-08-19 23:22:01 -0400

    run a simple hostmask test

--- server/server.py
+++ server/server.py
@@ -1,1 +1,13 @@
-# ...
+import sys
+
+from common.hostmask import *
+
+line = sys.stdin.readline()
+
+hs = hostmask(line)
+if hs is None:
+	print("invalid hostname {}".format(line))
+else:
+	print("nick: {}".format(hs.nick))
+	print("user: {}".format(hs.user))
+	print("hostt: {}".format(hs.host))
