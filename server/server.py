6d8943f49aaeb69352b810f2bafc79caa72f5d83
Wyatt Lindquist <git.wquist@gmail.com>
2016-08-21 16:13:55 -0400

    add documentation to server and user

--- server/server.py
+++ server/server.py
@@ -6,10 +6,28 @@ from user import *
 from common.command import *
 
 class Server:
+	"""
+	An instance of an IRC server.
+	Manages users and channels, and handles received messages.
+	"""
 
 	class Listener(Thread):
+		"""
+		A single, threaded, connection to a client.
+		Waits for data to be sent from the client, and passes it to the server
+		for processing.
+		"""
 
 		def __init__(self, serv, addr, conn):
+			"""
+			Initialize the client listener and its thread.
+
+			:param serv: The server this listener was spawned from.
+			:param addr: The addres the connection was opened on.
+			:param conn: The socket connection itself.
+			:return: None
+			"""
+
 			Thread.__init__(self)
 			self._connection = conn
 			self._address = addr
@@ -17,15 +35,24 @@ class Server:
 			self._server = serv
 
 		def run(self):
+			"""
+			Begin the messaging loop waiting for data from the client.
+
+			:return: None
+			"""
+
 			while True:
+				# IRC messages can be a maximum of 512 characters
 				data = self._connection.recv(512)
 				if not data:
 					break
 
+				# The server method will return True when the client quits
 				done = self._server.handle_message(self, data)
 				if done:
 					break
 
+			# Explicitly finish the connection - any remaining data will be sent
 			self._connection.close()
 
 		@property
@@ -33,23 +60,50 @@ class Server:
 			return self._address
 
 	def __init__(self):
+		"""
+		Create a new IRC server.
+
+		:return: None
+		"""
+
 		self._users = {}
 
 	def start(self, ip, port):
+		"""
+		Begin listening for client connections at the given address.
+
+		:param ip: The IP address to listen on.
+		:param port: The port to listen on. Should be 6660-6669 or 7000.
+		:return: None
+		"""
+
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 
 		sock.bind((ip, port))
 		while True:
+			# Listen with no queued connections - will block
 			sock.listen(0)
+			# A connection has been acquired - get its info
 			(conn, addr) = sock.accept()
 
 			(ip, _) = addr
+			# All users are pending until the nick and host info is sent
 			self._users[ip] = PendingUser(ip)
 
+			# Begin waiting for data from this client
 			Server.Listener(self, addr, conn).start()
 
 	def handle_message(self, source, data):
+		"""
+		Perform an action based on the message received.
+		Handle passing messages, managing users/channels, etc.
+
+		:param source: The Listener the data was received on.
+		:param data: The raw data received from the client.
+		:return: True, if the connection should now close; False otherwise.
+		"""
+
 		(ip, _) = source.address
 		usr = self._users[ip]
 
@@ -57,6 +111,7 @@ class Server:
 		if (msg.command == Command.QUIT):
 			return True
 
+		# Most normal commands do not end with finishing the connection
 		return False
 
 	@property
--- server/user.py
+++ server/user.py
@@ -1,13 +1,35 @@
 from common.hostmask import *
 
 class User:
+	"""
+	The common traits of a user, with or without all required information.
+	"""
 
 	def is_registered(self):
+		"""
+		This function should be overriden to specifiy whether a host can send
+		messages, or still need to send USER/NICK info.
+
+		:return: A boolean representing the registered state.
+		"""
+
 		raise NotImplementedError
 
 class PendingUser(User):
+	"""
+	The initial state of a user.
+	The user can only leave this state once USER/NICK info is sent.
+	"""
 
 	def __init__(self, addr):
+		"""
+		Create a new pending user.
+		The hostname is always available immeadiately.
+
+		:param addr: The hostname of the client.
+		:return: None
+		"""
+
 		self.nickname = None
 		self.username = None
 
@@ -17,6 +39,13 @@ class PendingUser(User):
 		return False
 
 	def is_complete(self):
+		"""
+		A pending user is complete once USER/NICK info is available.
+		A user can only be promoted to registered once they are complete.
+
+		:return: A boolean representing whether or not the user is complete.
+		"""
+
 		if self.nickname is None:
 			return False
 		if self.username is None:
@@ -29,12 +58,32 @@ class PendingUser(User):
 		return self._hostname
 
 class RegisteredUser(User):
+	"""
+	A user that has USER/NICK info.
+	A registered user can send messages and use the rest of IRC functionality.
+	"""
 
 	@staticmethod
 	def from_pending(pu):
+		"""
+		Promote a pending user to a registered user.
+
+		:param pu: The pending user to promote.
+		:return: A new registered user with the information of the pending user.
+		"""
+
 		return RegisteredUser(pu.nickname, pu.username, pu.hostname)
 
 	def __init__(self, nick, user, host):
+		"""
+		Create a new registered user with the given identification info.
+
+		:param nick: The user nickname.
+		:param user: The user username.
+		:param host: The user hostname.
+		:return: None
+		"""
+
 		self._hostmask = Hostmask(nick, user, host)
 
 	def is_registered(self):
