77745d048d8d649a26e2cdd60b57808ba4a417e1
Wyatt Lindquist <git.wquist@gmail.com>
2016-08-21 15:06:00 -0400

    start encapsulating server functionality

--- server/server.py
+++ server/server.py
@@ -2,53 +2,33 @@ import socket
 from threading import Thread
 from SocketServer import ThreadingMixIn
 
-from common.command import *
 from user import *
 
-def handle_message(u, raw):
-	m = Command(raw)
+class Server:
 
-	quit = False
-	if m.command == Command.NICK:
-		u.nickname = m.arguments[0]
-	if m.command == Command.USER:
-		u.username = m.arguments[0]
-	if m.command == Command.QUIT:
-		quit = True
+	class Listener(Thread):
 
-	hm = u.make_hostmask()
-	if hm is not None:
-		output = "{} is now registered!".format(format(hm))
-		print(output)
+		def __init__(self, conn):
+			Thread.__init__(self)
 
-	return quit
+			self._connection = conn
 
-class ClientListener(Thread):
+		def run(self):
+			while True:
+				data = self._connection.recv(512)
+				if not data:
+					break
 
-	def __init__(self, usr, conn):
-		Thread.__init__(self)
+				# handle the message
 
-		self._user = usr
-		self._connection = conn
+	def start(self, ip, port):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 
-	def run(self):
+		sock.bind((ip, port))
 		while True:
-			data = self._connection.recv(512)
-			if not data:
-				break
+			sock.listen(0)
+			(conn, addr) = sock.accept()
 
-			quit = handle_message(self._user, data)
-			if quit:
-				break
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-
-sock.bind(('127.0.0.1', 7000))
-while True:
-	sock.listen(0)
-	(conn, addr) = sock.accept()
-
-	user = User(addr)
-	listener = ClientListener(user, conn)
-	listener.start()
+			# create requesting user
+			Listener(conn).start()
